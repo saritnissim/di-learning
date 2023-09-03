0a72beb5205cf4fdd05785dc66c8c3dbe49c308d
saritnissim <saritbassal@gmail.com>
2023-09-03 20:13:09 +0300

    ninja.py

---
+++ Week2/Day1/ExercisesXPNinja/ninja.py
@@ -0,0 +1,48 @@
+# # Exercise 1: 
+# # Done
+
+# # Exercise 2:
+# # 1. vi ~/.zshrc 
+# # 2. alias py = python3
+# # 3. source ~/.zshrc 
+
+# # Exercise 3:
+# print("True, True, False, False, True, False")
+# x = (1 == True)
+# y = (1 == False)
+# a = True + 4
+# b = False + 10
+
+# print("x is", x)
+# print("y is", y)
+# print("a:", a)
+# print("b:", b)
+
+# # Exercise 4:
+# my_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+#            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+#            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+#            laboris nisi ut aliquip ex ea commodo consequat. 
+#            Duis aute irure dolor in reprehenderit in voluptate velit 
+#            esse cillum dolore eu fugiat nulla pariatur. 
+#            Excepteur sint occaecat cupidatat non proident, 
+#            sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+
+# print(len(my_text))
+
+
+# # Exercise 5:
+# longest = 0
+
+# while True:
+#     my_input = input("Input the longest sentence you can without the character “A”. Type q to quit ")
+#     sentence = my_input.lower()
+#     if sentence == "q" or "a" in sentence:
+#         print("Goodbye")
+#         break
+#     else:
+#         if len(sentence) > longest:
+#             longest = len(sentence)
+#             print(f"New record of {longest} characters")
+   
+       
