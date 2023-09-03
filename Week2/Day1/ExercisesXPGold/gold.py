035d1c9d69afd9926c64cfa441d8e15ab05ab52c
saritnissim <saritbassal@gmail.com>
2023-09-03 20:12:02 +0300

    Create gold.py

---
+++ Week2/Day1/ExercisesXPGold/gold.py
@@ -0,0 +1,14 @@
+# # Exercise 1
+# print("Hello World\n"*4 + "I love Python\n"*4)
+# # Exercise 2:
+# month_str = input ("Input a month (1 to 12) ")
+# month = int(month_str)
+
+# if month in range(3,6):
+#     print("Spring!")
+# elif month in range(6,9):
+#     print("Summer!")
+# elif month in range(9,12):
+#     print("Autumn!")
+# elif month == 12 or month in range(1,3):
+#     print("Winter!")
