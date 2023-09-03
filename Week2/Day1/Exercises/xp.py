e1db48c4bf31e3f57080a0d943c78b60334da85f
saritnissim <saritbassal@gmail.com>
2023-09-03 20:22:15 +0300

    Exercises XP

---
+++ Week2/Day1/Exercises/xp.py
@@ -0,0 +1,52 @@
+# # Exercise 1: 
+# for x in range(5):
+#     print("Hello World")
+
+# # Exercise 2:
+# print(99**3*8)
+
+# # Exercise 3:
+# print("False, True, Error, False, False")
+
+# # Exercise 4:
+# computer_brand = "Apple"
+# print(f"I have a(n) {computer_brand} computer.")
+
+# # Exercise 5:
+# name = "Sarit"
+# age = 33
+# shoe_size = 37
+# info = f"My name is {name}. I am {age} years old and I am a size {shoe_size} in shoe size"
+# print(info)
+
+# # Exercise 6:
+# a = 15
+# b = 10
+
+# if(a>b):
+#     print(f"{a} is greater than {b} -> Hello World")
+
+# # Exercise 7:
+# number_str = input("Give me a number ")
+# number = int(number_str)
+# if number == 0:
+#     print("Error. Your number is not valid")
+# elif number % 2 == 0:
+#     print("Your number is even")
+# else:
+#     print("Your number is odd")
+
+# # Exercise 8:
+# my_name = "Sarit"
+# name = input("What is your name? ")
+# if name == my_name:
+#     print("Ha! That's funny...we have the same name")
+
+# # Exercise 9:
+# height = input("What is your height in inches ")
+# height_cm = int(height) * 2.54
+
+# if height_cm > 145:
+#     print("You are tall enough to ride.")
+# else: 
+#     print("You need to grow some more")
